64bf4506d417f4b62085f66944c02bc56d562e31
João Simões <gravity00@users.noreply.github.com>
2023-01-11 17:53:16 +0000

    core parser options

---
+++ src/SimpleSoft.Gs1Parser/Gs1ParserOptions.cs
@@ -0,0 +1,17 @@
+﻿namespace SimpleSoft.Gs1Parser;
+
+/// <summary>
+/// Represents the options user by GS1 parsers.
+/// </summary>
+public class Gs1ParserOptions
+{
+    /// <summary>
+    /// Character to be used as the FNC1 separator. Defaults to ';'.
+    /// </summary>
+    public char Separator { get; set; } = ';';
+
+    /// <summary>
+    /// Default options for GS1 parsers.
+    /// </summary>
+    public static readonly Gs1ParserOptions Default = new();
+}
---
+++ tests/SimpleSoft.Gs1Parser.Tests/Gs1ParserOptionsTests.cs
@@ -0,0 +1,21 @@
+﻿namespace SimpleSoft.Gs1Parser;
+
+public class Gs1ParserOptionsTests
+{
+    [Fact]
+    public void Constructor_NoParameters_DefaultPropertyValues()
+    {
+        var options = new Gs1ParserOptions();
+
+        Assert.Equal(';', options.Separator);
+    }
+
+    [Fact]
+    public void DefaultSingleton_DefaultPropertyValues()
+    {
+        var options = Gs1ParserOptions.Default;
+
+        Assert.NotNull(options);
+        Assert.Equal(';', options.Separator);
+    }
+}
