e9866532589702fef3189cc3a99b6ae9ad128dd6
João Simões <gravity00@users.noreply.github.com>
2023-01-11 17:38:01 +0000

    immutable class representing a GS1 application identifier

---
+++ src/SimpleSoft.Gs1Parser/Gs1ApplicationIdentifier.cs
@@ -0,0 +1,40 @@
+﻿namespace SimpleSoft.Gs1Parser;
+
+/// <summary>
+/// Represents a GS1 application identifier
+/// </summary>
+public class Gs1ApplicationIdentifier
+{
+    /// <summary>
+    /// Creates a new instance
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="prefix"></param>
+    /// <param name="dataContent"></param>
+    /// <exception cref="ArgumentNullException"></exception>
+    public Gs1ApplicationIdentifier(
+        string value,
+        string prefix,
+        string dataContent
+    )
+    {
+        Value = value ?? throw new ArgumentNullException(nameof(value));
+        Prefix = prefix ?? throw new ArgumentNullException(nameof(prefix));
+        DataContent = dataContent ?? throw new ArgumentNullException(nameof(dataContent));
+    }
+
+    /// <summary>
+    /// Application identifier original value
+    /// </summary>
+    public string Value { get; }
+
+    /// <summary>
+    /// Application identifier prefix
+    /// </summary>
+    public string Prefix { get; }
+
+    /// <summary>
+    /// Application identifier data content
+    /// </summary>
+    public string DataContent { get; }
+}
