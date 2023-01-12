ecf6316092a1493fde3d28dc8a3ea24a84dd1ad0
João Simões <gravity00@users.noreply.github.com>
2023-01-12 16:55:05 +0000

    parser implementation with basic tests

---
+++ src/SimpleSoft.Gs1Parser/IGs1.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleSoft.Gs1Parser;
+
+/// <summary>
+/// Represents a GS1 entity
+/// </summary>
+public interface IGs1 : IReadOnlyDictionary<string, Gs1ApplicationIdentifier>
+{
+    /// <summary>
+    /// GS1 original value
+    /// </summary>
+    public string RawValue { get; }
+}
