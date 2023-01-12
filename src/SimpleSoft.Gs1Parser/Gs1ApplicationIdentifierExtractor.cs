f61704140e5e980b31bf5ef2a79df25f321f2af9
João Simões <gravity00@users.noreply.github.com>
2023-01-12 12:21:33 +0000

    delegate function do extract application identifiers from GS1 strings

---
+++ src/SimpleSoft.Gs1Parser/Gs1ApplicationIdentifierExtractor.cs
@@ -0,0 +1,16 @@
+﻿namespace SimpleSoft.Gs1Parser;
+
+/// <summary>
+/// Function responsible for extracting a given application identifier.
+/// </summary>
+/// <param name="value">The GS1 string value</param>
+/// <param name="startIndex">The start index to lookup for the application identifier</param>
+/// <param name="prefix">Application identifier prefix</param>
+/// <param name="separator">Character representing the FNC1 separator</param>
+/// <returns></returns>
+public delegate Gs1ApplicationIdentifier Gs1ApplicationIdentifierExtractor(
+    string value,
+    int startIndex,
+    string prefix,
+    char separator
+);
