8612eb4b51e12dd67185d9c89215494615b9bbdf
João Simões <gravity00@users.noreply.github.com>
2023-01-20 16:29:10 +0000

    parser interface and throwing FormatException instead of InvalidOperationException

---
+++ src/SimpleSoft.Gs1Parser/IGs1Parser.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleSoft.Gs1Parser;
+
+/// <summary>
+/// Represents a GS1 parser.
+/// </summary>
+public interface IGs1Parser
+{
+    /// <summary>
+    /// Parses a given GS1 string retuning a collection of application identifiers
+    /// indexed by their prefix.
+    /// </summary>
+    /// <param name="rawValue">The GS1 string to parse</param>
+    /// <returns>The collection of application identifiers</returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    /// <exception cref="ArgumentException"></exception>
+    /// <exception cref="FormatException"></exception>
+    IGs1 Parse(string rawValue);
+}
